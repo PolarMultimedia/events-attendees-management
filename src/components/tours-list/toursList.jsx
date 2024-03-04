c78b14824669277559986b9001a279eafa2f746a
Diego Flores <dfmonroy07@gmai.com>
2024-03-04 17:33:11 -0600

    updated get info

--- src/components/tours-list/toursList.jsx
+++ src/components/tours-list/toursList.jsx
@@ -1,41 +1,59 @@
 import { Link } from 'react-router-dom';
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 function ToursList ({tours}) {
 
     return (
         <>
             <div className="text-center px-4 pt-16 pb-10 sm:px-2 lg:px-8 lg:pt-24 lg:pb-10">
-                <h2 className="text-3xl font-bold tracking-tight text-morado_abalat sm:text-4xl uppercase">Lista de Giras</h2>
+                <h2 className="text-3xl font-bold tracking-tight text-morado_abalat sm:text-4xl uppercase">Lista de Eventos</h2>
             </div>
             {
                 tours.length !== 0 ?
-                <div className="px-4 py-3 text-right sm:px-6 ">
-                    <div className="overflow-hidden bg-white shadow sm:rounded-md">
-                        <ul className="divide-y divide-gray-200">
-                            {tours.map((tour) => (
-                            <li key={tour.name}>
-                                <Link to={'/eventos/'+tour.id} className="block hover:bg-gray-50">
-                                    <div className="flex items-center px-4 py-4 sm:px-6">
-                                        <div className="flex min-w-0 flex-1 items-center">
-                                            <div className="min-w-0 flex-1 px-4 md:grid md:gap-4">
-                                                <div>
-                                                    <p className="truncate text-sm text-start font-medium text-gray-900">{tour.name}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        </div>
-                                    </div>
-                                </Link>
-                            </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div> :
+                <div className='p-8'>
+                    <div className="mt-8 flow-root">
+                        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead className="bg-gray-50">
+                                <tr>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                    Evento
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Total de Fechas
+                                    </th>
+                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span className="sr-only">Ver Fechas</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                {tours.map((tour) => (
+                                    <tr key={tour.id}>
+                                        <td className="py-4 pl-6 pr-3 text-sm font-medium text-gray-900 md:pl-4">
+                                            {tour.name}
+                                        </td>
+                                        <td className="py-4 pl-6 pr-3 text-sm font-medium text-gray-900 md:pl-4">
+                                            {tour.count}
+                                        </td>
+                                        <td className="relative py-4 pl-3 pr-6 text-right text-sm font-medium md:pr-4">
+                                            <Link to={'/eventos/'+tour.id} className="text-highlight hover:text-lowContrast">
+                                            Ver fechas<span className="sr-only">, {tour.name}</span>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                        </div>
+                    </div> 
+                </div>
+                :
                 <div className="text-center px-4 pt-16 pb-10 sm:px-2 lg:px-8 lg:pt-24 lg:pb-10">
-                    <h3 className="text-xl font-bold tracking-tight text-morado_abalat sm:text-2xl">No hay tours registrados</h3>
+                    <h3 className="text-xl font-bold tracking-tight text-morado_abalat sm:text-2xl">No hay Eventos registrados</h3>
                 </div>
             }
         </>
