391a31ef42ea04bc5bff859bd8a95c557c83c9c8
Diego Flores <dfmonroy07@gmai.com>
2024-03-06 12:53:42 -0600

    updated layout

--- src/components/events-list/events-list.jsx
+++ src/components/events-list/events-list.jsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 function EventsList ({events, tourName}) {
     return (
         <>
             <div className="text-center px-4 pt-16 pb-10 sm:px-2 lg:px-8 lg:pt-24 lg:pb-10">
-                <h2 className="text-3xl font-bold tracking-tight text-morado_abalat sm:text-4xl uppercase">Lista de Eventos de "{tourName}"</h2>
+                <h2 className="text-3xl font-bold tracking-tight text-morado_abalat sm:text-4xl uppercase">Fechas de "{tourName}"</h2>
             </div>
             {
                 events.length !== 0 ?
-                <div className="px-4 py-3 text-right sm:px-6 ">
-                    <div className="overflow-hidden bg-white shadow sm:rounded-md">
-                        <ul className="divide-y divide-gray-200">
-                            {events.map((event) => (
-                            <li key={event.city}>
-                                <Link to={'/invitados/'+event.id} className="block hover:bg-gray-50">
-                                    <div className="flex items-center px-4 py-4 sm:px-6">
-                                        <div className="flex min-w-0 flex-1 items-center">
-                                            <div className="min-w-0 flex-1 px-4 md:grid md:gap-4">
-                                                <div>
-                                                    <p className="truncate text-sm text-start font-medium text-gray-900">{event.name}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        </div>
-                                    </div>
-                                </Link>
-                            </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div> : 
+                <div className='p-8'>
+                    <div className="mt-8 flow-root">
+                        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead className="bg-gray-50">
+                                <tr>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                    Fecha
+                                    </th>
+                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span className="sr-only">Ver Invitados</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                {events.map((event) => (
+                                    <tr key={event.id}>
+                                        <td className="py-4 pl-6 pr-3 text-sm font-medium text-gray-900 md:pl-4">
+                                            {event.name}
+                                        </td>
+                                        <td className="relative py-4 pl-3 pr-6 text-right text-sm font-medium md:pr-4">
+                                            <Link to={'/invitados/'+event.id} className="text-highlight hover:text-lowContrast">
+                                            Ver fechas<span className="sr-only">, {event.name}</span>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                        </div>
+                    </div> 
+                </div>
+                : 
                 <div className="text-center px-4 pt-16 pb-10 sm:px-2 lg:px-8 lg:pt-24 lg:pb-10">
-                    <h3 className="text-xl font-bold tracking-tight text-morado_abalat sm:text-2xl">No hay eventos registrados</h3>
+                    <h3 className="text-xl font-bold tracking-tight text-morado_abalat sm:text-2xl">No hay fechas registradas</h3>
                 </div>
             }
             
